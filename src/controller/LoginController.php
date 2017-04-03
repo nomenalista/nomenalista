37614c2d9da02a49561c11b00fa42be9f6127874
Junior Oliveira <arojunior@gmail.com>
2017-04-03 18:36:10 -0300

    login method moved to Users model

--- src/controller/LoginController.php
+++ src/controller/LoginController.php
@@ -2,43 +2,22 @@
 namespace NomenaLista\controller;
 
 use \NomenaLista\model\Users as Users;
-use \NomenaLista\lib\ErrorsLib as Errors;
 
 class LoginController
 {
 
   protected $Users;
-  public $Errors;
 
   public function __construct()
   {
        $this->Users = new Users;
-       $this->Errors = new Errors;
   }
 
   public function index($request, $response)
   {
       $data = $request->getParsedBody();
 
-      return $response->withJson(self::loginUser($data));
-  }
-
-  private function loginUser($data)
-  {
-      if ( ! $this->Users->exists($data['email'])) {
-          return $this->Errors->getError(1);
-      }
-
-      if ( ! $this->Users->validatePassword($data)) {
-          return $this->Errors->getError(2);
-      }
-
-      $data = $this->Users->fetch();
-      
-      return [
-          'isLogged' => true,
-          'token'    => $this->Users->Lib->hash($data['id'])
-      ];
+      return $response->withJson($this->Users->login($data));
   }
 
 }
--- src/model/Users.php
+++ src/model/Users.php
@@ -4,17 +4,20 @@ namespace NomenaLista\model;
 use NomenaLista\model\AppModel;
 use NomenaLista\model\contracts\UsersInterface;
 use NomenaLista\lib\UsersLib;
+use NomenaLista\lib\ErrorsLib as Errors;
 
 class Users extends AppModel implements UsersInterface
 {
     public $pk = 'email';
     public $Lib;
+    public $Errors;
 
     public function __construct()
     {
         parent::__construct();
 
         $this->Lib = new UsersLib;
+        $this->Errors = new Errors;
     }
 
     public function store($data)
@@ -27,12 +30,30 @@ class Users extends AppModel implements UsersInterface
 
     }
 
+    public function login($data)
+    {
+        if ( ! $this->exists($data['email'])) {
+            return $this->Errors->getError(1);
+        }
+
+        if ( ! $this->validatePassword($data)) {
+            return $this->Errors->getError(2);
+        }
+
+        $data = $this->fetch();
+
+        return [
+            'isLogged' => true,
+            'token'    => $this->Lib->hash($data['id'])
+        ];
+    }
+
     public function validatePassword($data)
     {
         return (password_verify($data['password'], $this->fetch()['password']));
     }
 
-    private function hashPassword($data)
+    public function hashPassword($data)
     {
         return array_merge($data, [
           'password' => $this->Lib->hash($data['password'])
