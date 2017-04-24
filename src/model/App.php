114b938e84563d6e8fd0e08813b3f4a0ead5f6ed
Junior Oliveira <arojunior@gmail.com>
2017-04-24 11:20:24 -0300

    .

--- src/model/App.php
+++ src/model/App.php
@@ -9,15 +9,15 @@ class App extends Model
     public $db_config;
 
     public function __construct()
-    {
-        parent::__construct();
-
+    {            
         $this->db_config = [
             'db_host' => getenv("DB_HOST"),
             'db_name' => getenv("DB_DATABASE"),
             'db_user' => getenv("DB_USERNAME"),
             'db_pass' => getenv("DB_PASSWORD")
         ];
+
+        parent::__construct();
     }
 
 }
